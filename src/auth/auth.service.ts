bd45d3c0a9e5408d2ee7724ba6b8c3d1160d9ead
Gabriel Cardoso <gr78787@gmail.com>
2023-04-28 16:33:14 -0300

    feat(auth) implement login, guard and localStrategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+    throw new Error('Method not implementeds.');
   }
 }
